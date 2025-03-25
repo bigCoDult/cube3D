9131e301bc8164d32c649e6a6dfb16997adef141
bigCoDult <qqqqmmss11@gmail.com>
2025-03-25 16:18:38 +0900

    [edit] 모든것을 담는 t_total

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -18,21 +18,36 @@ typedef struct s_cordi
 	int	col;
 }	t_cordi;
 
-typedef struct s_data
+typedef enum e_direction
+{
+	NORTH = 0,
+	EAST = 1,
+	SOUTH = 2,
+	WEST = 3
+}	t_direction;
+
+typedef struct s_image_info
 {
 	char *ceiling; /* "숫자,숫자,숫자" 이 형태의 문자열*/
 	char *floor;
-	int *floor; /* 파일 경로*/
-	char *north;
+	char *north; /* 파일 경로*/
 	char *south;
 	char *east;
 	char *west;
-} t_data;
+} t_image_info;
 
 typedef struct s_parsed
 {
-	t_data data;
+	t_image_info image_info;
 	char **map;
+	t_cordi *player;
+	int direction; // 플레이어가 바라보고 있는 방향 = 게임을 시작했을때 보여야 하는 타일
 } t_parsed;
+
+typedef struct s_total // minishell에서의 t_shell과 같음
+{
+	t_parsed *parsed;
+	// 필요한것 추가 바랍니다
+} t_total;
 
 #endif /* CUB3D_H */
