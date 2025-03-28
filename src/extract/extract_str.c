f7daf636e1736a14c3455fc640460201af4a38fe
bigCoDult <qqqqmmss11@gmail.com>
2025-03-28 20:15:11 +0900

    [edit] extract_str()에서 main()을 분리

--- src/extract/extract_str.c
+++ src/extract/extract_str.c
@@ -1,7 +1,6 @@
 #include "../../inc/cub3d.h"
 // #include <cub3d.h>
 
-
 static int	count_space(char *str)
 {
 	int	i;
@@ -14,7 +13,7 @@ static int	count_space(char *str)
 	return (i);
 }
 
-int find_i_key(char *file, const char *key)
+static int	find_i_key(char *file, const char *key)
 {
 	int	i;
 	int	k;
@@ -32,7 +31,7 @@ int find_i_key(char *file, const char *key)
 	return (-1);
 }
 
-char *extract_value(const char *key, char *file)
+static char	*extract_value(const char *key, char *file)
 {
 	int	i_key;
 	int	i_start;
@@ -45,7 +44,8 @@ char *extract_value(const char *key, char *file)
 		|| !is_space(file[i_key + ft_strlen(key)]))
 		return (NULL);
 	i_start = i_key + ft_strlen(key);
-	if(count_space(file + i_key + ft_strlen(key)) == 0 || count_space(file + i_key + ft_strlen(key)) == -1)
+	if (count_space(file + i_key + ft_strlen(key)) == 0 \
+		|| count_space(file + i_key + ft_strlen(key)) == -1)
 		return (NULL);
 	i_start += count_space(file + i_key + ft_strlen(key));
 	i_end = i_start;
@@ -54,43 +54,55 @@ char *extract_value(const char *key, char *file)
 	return (ft_strndup(file + i_start, i_end - i_start));
 }
 
-
-
-
-
-
-
-
-
-int main(void)
+static int	extract_str(int fd, t_total *total)
 {
-	t_total *total;
-	total = (t_total *)ft_calloc(sizeof(t_total), 1);
-	total->parsed = malloc(sizeof(t_parsed));
-	total->parsed->extracted_str = malloc(sizeof(t_extracted_str));
-	int fd = open("../../img/map.cub", O_RDONLY);
-	char *file = file_to_str(fd);
+	char	*file;
+
+	file = file_to_str(fd);
 	total->parsed->extracted_str->north = extract_value("NO", file);
 	total->parsed->extracted_str->south = extract_value("SO", file);
 	total->parsed->extracted_str->west = extract_value("WE", file);
 	total->parsed->extracted_str->east = extract_value("EA", file);
 	total->parsed->extracted_str->floor = extract_value("F", file);
 	total->parsed->extracted_str->ceiling = extract_value("C", file);
-	
-	// printf("%s\n\n\n", file);
+	if (!total->parsed->extracted_str->north || !total->parsed->extracted_str->south \
+		|| !total->parsed->extracted_str->west || !total->parsed->extracted_str->east \
+		|| !total->parsed->extracted_str->floor || !total->parsed->extracted_str->ceiling)
+	{
+		if (total->parsed->extracted_str->north)
+			free(total->parsed->extracted_str->north);
+		if (total->parsed->extracted_str->south)
+			free(total->parsed->extracted_str->south);
+		if (total->parsed->extracted_str->west)
+			free(total->parsed->extracted_str->west);
+		if (total->parsed->extracted_str->east)
+			free(total->parsed->extracted_str->east);
+		if (total->parsed->extracted_str->floor)
+			free(total->parsed->extracted_str->floor);
+		if (total->parsed->extracted_str->ceiling)
+			free(total->parsed->extracted_str->ceiling);
+		free(total->parsed->extracted_str);
+	}
+	free(file);
+	return (0);
+}
 
+int	main(void)
+{
+	t_total	*total;
+	int		fd;
 
+	fd = open("../../img/map.cub", O_RDONLY);
+	total = (t_total *)ft_calloc(sizeof(t_total), 1);
+	total->parsed = malloc(sizeof(t_parsed));
+	total->parsed->extracted_str = malloc(sizeof(t_extracted_str));
+	extract_str(fd, total);
 	printf("north: %s\n", total->parsed->extracted_str->north);
 	printf("south: %s\n", total->parsed->extracted_str->south);
 	printf("west: %s\n", total->parsed->extracted_str->west);
 	printf("east: %s\n", total->parsed->extracted_str->east);
 	printf("floor: %s\n", total->parsed->extracted_str->floor);
 	printf("ceiling: %s\n", total->parsed->extracted_str->ceiling);
-	
-	
-	
-	
-	free(file);
 	free(total->parsed->extracted_str->north);
 	free(total->parsed->extracted_str->south);
 	free(total->parsed->extracted_str->west);
