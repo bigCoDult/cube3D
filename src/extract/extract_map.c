6ef156b5375e00c0dfef3044f9bb23c05ac18aa8
bigCoDult <qqqqmmss11@gmail.com>
2025-03-29 20:37:57 +0900

    [feat] 파일에서 char **map 추출

---
+++ src/extract/extract_map.c
@@ -0,0 +1,95 @@
+#include "../../inc/cub3d.h"
+
+static int	is_map_part(char c)
+{
+	if (c == '1' || c == '0' || c == ' ' || c == '\n' \
+	|| c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		return (1);
+	return (0);
+}
+
+static int	find_map_start(char *file, int i)
+{
+	while (file[i] != '\0')
+		i++;
+	i--;
+	while (i > 0 && is_map_part(file[i]))
+		i--;
+	while (file[i] != '\n')
+		i++;
+	while (file[i] == '\n')
+		i++;
+	return (i);
+}
+
+static void	set_rowcol(char *file, int i, int *row, int *col)
+{
+	int	line_i;
+
+	line_i = 0;
+	*col = 1;
+	while (file[i] != '\0')
+	{
+		if (file[i] == '\n')
+		{
+			(*col)++;
+			line_i = 0;
+		}
+		else
+		{
+			line_i++;
+			if (line_i > *row)
+				*row = line_i;
+		}
+		i++;
+	}
+	return ;
+}
+
+static void	*malloc_map(t_total *total, int row, int col)
+{
+	total->parsed->map = (char **)malloc(sizeof(char *) * (col + 1));
+	total->parsed->map[col] = NULL;
+	if (!total->parsed->map)
+		return (NULL);
+	col--;
+	while (col >= 0)
+	{
+		total->parsed->map[col] = (char *)malloc(sizeof(char) * (row + 1));
+		if (!total->parsed->map[col])
+			return (NULL);
+		ft_memset(total->parsed->map[col], ' ', row);
+		total->parsed->map[col][row] = '\0';
+		col--;
+	}
+	return (total->parsed->map);
+}
+
+void	extract_map(char *file, t_total *total)
+{
+	int	i;
+	int	row;
+	int	col;
+
+	i = 0;
+	row = 0;
+	col = 0;
+	i = find_map_start(file, i);
+	set_rowcol(file, i, &row, &col);
+	row = 0;
+	col = 0;
+	while (file[i] != '\0')
+	{
+		if (file[i] == '\n')
+		{
+			col++;
+			row = 0;
+		}
+		else
+		{
+			total->parsed->map[col][row] = file[i];
+			row++;
+		}
+		i++;
+	}
+}
