aab346ca8237709875124a451887b397653517ae
yutsong16 <tm1361@naver.com>
2025-03-30 13:21:43 +0900

    [refactor] 맥 환경을 위한 수정사항 - 컴파일 명령어 추가(기존 명령어도 보존) - 컴파일 에러 수정

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -1,8 +1,8 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <X11/X.h>
-# include <X11/keysym.h>
+// # include <X11/X.h>
+// # include <X11/keysym.h>
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
 # include <stdio.h>
