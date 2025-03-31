2e2e06812fda54ece257a52673d291d1d9d65f17
bigCoDult <qqqqmmss11@gmail.com>
2025-03-31 15:37:41 +0900

    [edit] 경로변경으로 인한 수정

--- src/dummy_main.c
+++ src/dummy_main.c
@@ -44,15 +44,15 @@ int	main(void)
 	printf("------------map end------------\n");
 	
 	
-	// total->mlx = malloc(sizeof(t_mlx));
-	// total->mlx->mlx_ptr = mlx_init();
-	// total->mlx->win_ptr = mlx_new_window(total->mlx->mlx_ptr, 1000, 500, "pokemon mountain");
-	// extract_img(total);
-	// mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->north, 0, 0);
-	// mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->south, 64, 0);
-	// mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->west, 0, 64);
-	// mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->east, 64, 64);
-	// mlx_loop(total->mlx->mlx_ptr);
+	total->mlx = malloc(sizeof(t_mlx));
+	total->mlx->mlx_ptr = mlx_init();
+	total->mlx->win_ptr = mlx_new_window(total->mlx->mlx_ptr, 300, 300, "pokemon mountain");
+	extract_img(total);
+	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->north, 0, 0);
+	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->south, 64, 0);
+	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->west, 0, 64);
+	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->east, 64, 64);
+	mlx_loop(total->mlx->mlx_ptr);
 	col = 0;
 	while (total->parsed->map[col] != NULL)
 	{
