d3fb0998a416956ebbd20ca69dfc386232fe6ece
bigCoDult <qqqqmmss11@gmail.com>
2025-04-09 09:33:27 +0900

    [edit] norm, extract_img.c

--- src/extract_img.c
+++ src/extract_img.c
@@ -1,9 +1,21 @@
-#include "../inc/cub3d.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extract_img.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 09:32:34 by sanbaek           #+#    #+#             */
+/*   Updated: 2025/04/09 09:32:59 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_atohex(char *hex)
+#include "cub3d.h"
+
+int	ft_atohex(char *hex)
 {
-	int i;
-	int result;
+	int	i;
+	int	result;
 
 	i = 0;
 	result = 0;
@@ -20,24 +32,25 @@ int ft_atohex(char *hex)
 	return (result);
 }
 
-char	*dec_to_hex(char *dec)
+static char	*dec_to_hex(char *dec)
 {
 	int		n;
 	char	*hex;
-	hex = malloc(sizeof(char) * 3);
 
+	hex = malloc(sizeof(char) * 3);
 	n = ft_atoi(dec);
 	hex[0] = "0123456789ABCDEF"[n / 16];
 	hex[1] = "0123456789ABCDEF"[n % 16];
 	hex[2] = '\0';
 	return (hex);
 }
-char	*convert_rgb_to_hex(char *str)
+
+static char	*convert_rgb_to_hex(char *str)
 {
-	char **rgb;
-	char *hex;
-	char *tmp;
-	
+	char	**rgb;
+	char	*hex;
+	char	*tmp;
+
 	rgb = ft_split(str, ',');
 	hex = ft_calloc(sizeof(char), 1);
 	tmp = dec_to_hex(rgb[0]);
@@ -55,27 +68,23 @@ char	*convert_rgb_to_hex(char *str)
 	free(rgb);
 	return (hex);
 }
-void extract_img(t_total *total)
+
+void	extract_img(t_total *total)
 {
-	int width;
-	int height;
-	printf("ceiling	: %s\n", total->parsed->extracted_str->ceiling);
-	printf("floor		: %s\n", total->parsed->extracted_str->floor);
+	t_cordi	length;
+
+	length = (t_cordi){64, 64};
 	total->parsed->image_info = malloc(sizeof(t_image_info));
-	total->parsed->image_info->ceiling = convert_rgb_to_hex(total->parsed->extracted_str->ceiling);
-	total->parsed->image_info->floor = convert_rgb_to_hex(total->parsed->extracted_str->floor);
-	printf("ceiling : %s\n", total->parsed->image_info->ceiling);
-	printf("floor : %s\n", total->parsed->image_info->floor);
-	printf("%d\n", ft_atohex(total->parsed->image_info->ceiling));
-	printf("%d\n", ft_atohex(total->parsed->image_info->floor));
-	width = 64;
-	height = 64;
-	total->parsed->image_info->north = mlx_xpm_file_to_image(total->mlx->mlx_ptr,total->parsed->extracted_str->north, &width, &height);
-	total->parsed->image_info->south = mlx_xpm_file_to_image(total->mlx->mlx_ptr,total->parsed->extracted_str->south, &width, &height);
-	total->parsed->image_info->west = mlx_xpm_file_to_image(total->mlx->mlx_ptr,total->parsed->extracted_str->west, &width, &height);
-	total->parsed->image_info->east = mlx_xpm_file_to_image(total->mlx->mlx_ptr,total->parsed->extracted_str->east, &width, &height);
-	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->north, 0, 0);
-	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->south, 64, 0);
-	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->west, 0, 64);
-	mlx_put_image_to_window(total->mlx->mlx_ptr, total->mlx->win_ptr, total->parsed->image_info->east, 64, 64);
+	total->parsed->image_info->ceiling = \
+		convert_rgb_to_hex(total->parsed->extracted_str->ceiling);
+	total->parsed->image_info->floor = \
+		convert_rgb_to_hex(total->parsed->extracted_str->floor);
+	total->parsed->image_info->north = mlx_xpm_file_to_image(total->mlx->\
+		mlx_ptr, total->parsed->extracted_str->north, &length.col, &length.row);
+	total->parsed->image_info->south = mlx_xpm_file_to_image(total->mlx->\
+		mlx_ptr, total->parsed->extracted_str->south, &length.col, &length.row);
+	total->parsed->image_info->west = mlx_xpm_file_to_image(total->mlx->\
+		mlx_ptr, total->parsed->extracted_str->west, &length.col, &length.row);
+	total->parsed->image_info->east = mlx_xpm_file_to_image(total->mlx->\
+		mlx_ptr, total->parsed->extracted_str->east, &length.col, &length.row);
 }
