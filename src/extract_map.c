88982837473b0ac29e8e9dce1d2caf8ed4ab88e1
bigCoDult <qqqqmmss11@gmail.com>
2025-04-15 17:25:25 +0900

    [feat] 맵 중간에 개행이 있으면 잘라서 맵으로 파싱하고 validation 부분에서 체크함

--- src/extract_map.c
+++ src/extract_map.c
@@ -6,7 +6,7 @@
 /*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 09:33:50 by sanbaek           #+#    #+#             */
-/*   Updated: 2025/04/13 12:46:26 by sanbaek          ###   ########.fr       */
+/*   Updated: 2025/04/15 17:22:35 by sanbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ int	find_map_start(char *file, int i)
 		i++;
 	i--;
 	while (i > 0 && is_map_part(file[i]))
+	{
+		if (file[i] == '\n' && file[i - 1] == '\n')
+			return (i);
 		i--;
+	}
 	while (file[i] != '\n')
 		i++;
 	while (file[i] == '\n')
